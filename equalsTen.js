fc3374fdd4db794866ccda7f8a1c9b3cffc82613
torrespain <javirorre86@gmail.com>
2018-06-04 12:43:37 -0700

    3 new ones

---
+++ equalsTen.js
@@ -0,0 +1,15 @@
+/*Write a function called "equalsTen".
+
+Given a number, "equalsTen" returns whether or not the given number is 10.
+
+var output = equalsTen(9);
+console.log(output); // --> false
+
+Starter Code :*/
+function equalsTen(num) {
+  if (num == 10) return true;
+  return false;
+}
+
+var output = equalsTen(9);
+console.log(output); // --> false
