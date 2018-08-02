d86373b4ea4e1a8ac23b5948d2c6e1576a1434f3
torrespain <javirorre86@gmail.com>
2018-08-02 09:29:25 -0700

    added some more

---
+++ getElementsLessThan100AtProperty.js
@@ -0,0 +1,32 @@
+/*Write a function called "getElementsLessThan100AtProperty".
+
+Given an object and a key, "getElementsLessThan100AtProperty" returns an array containing all the elements of the array located at the given key that are less than 100.
+
+Notes:
+* If the array is empty, it should return an empty array.
+* If the array contains no elements less than 100, it should return an empty array.
+* If the property at the given key is not an array, it should return an empty array.
+* If there is no property at the key, it should return an empty array.
+
+var obj = {
+  key: [1000, 20, 50, 500]
+};
+var output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output); // --> [20, 50]
+
+Starter Code :*/
+function getElementsLessThan100AtProperty(obj, key) {
+  let elements = [];
+  for (let prop in obj[key]) {
+    if (obj[key][prop]<100) {
+      elements.push(obj[key][prop]);
+    }
+  }
+  return elements;
+}
+
+var obj = {
+  key: [1000, 20, 50, 500]
+};
+var output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output); // --> [20, 50]
