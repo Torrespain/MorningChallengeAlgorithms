1e0d7e4ef1af12eb344d3a00f16ac0a25800002c
Torrespain <javirorre86@gmail.com>
2018-09-04 09:03:09 -0700

    3 more

---
+++ multiply.js
@@ -0,0 +1,16 @@
+/*Write a function called "multiply".
+
+Given 2 numbers, "multiply" returns their product.
+
+Notes:
+* It should not use the multiply operator (*).\
+
+var output = multiply(4, 7);
+console.log(output); // --> 28
+
+Starter Code :*/
+function multiply(num1, num2) {
+  return (num1*num2);
+}
+var output = multiply(4, 7);
+console.log(output); // --> 28
